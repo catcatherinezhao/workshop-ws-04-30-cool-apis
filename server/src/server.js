be102e68395adda6c136b9b0a08c827ee5114273
Catherine Zhao <catherine.zhao.20@dartmouth.edu>
2019-04-30 15:13:38 -0400

    finished workshop

--- server/src/server.js
+++ server/src/server.js
@@ -33,7 +33,7 @@ app.post('/', upload.single('file'), (req, res, next) => {
     body: fs.createReadStream(req.file.path),
     auth: {
       user: 'apikey',
-      pass: '<YOUR API KEY>',
+      pass: 'YHQYSubQfcXc9wXaioq81l6VKQ66aUXnpMBySP6yl04w',
     },
   }, (error, response, body) => {
     if (error) {
